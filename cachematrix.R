a7df8fcc98c715a6b71a44b5c84b5df4be773610
jessicafalbuquerque <jessicafalbuquerque@gmail.com>
2021-06-06 18:30:15 -0300

    Resolution of Assignment 2 of the R Programming course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  The following two functions manipulate the lexical scope of the r language 
+## to preserve the state of an object in R in a different environment than the 
+## one created and cache the result of an matrix operation.
 
-## Write a short comment describing this function
+## This function calculates the inverse of an matrix (x) and caches the result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function recovers the matrix inverse calculation performed by the 
+## previous function. If the result is not available in cache, the function 
+## calculates the inverse and returns the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getsolve()
+      if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setsolve(m)
+      m
 }
+ 
+
